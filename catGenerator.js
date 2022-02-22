349334c9346546fb544b1a4b8a541529ff675671
abhi271188 <abhikumar271188@gmail.com>
2022-02-22 11:27:03 +0100

    JS file of Project-2
    Create a function 'catGenerator' that generates the same image of cat while clicking on the button.

---
+++ catGenerator.js
@@ -0,0 +1,7 @@
+document.querySelector('#cat-generator-button-primary').addEventListener('click', catGenerator);
+function catGenerator()
+{
+    let catImage = document.createElement('img');
+    catImage.src = "https://s3.us-west-2.amazonaws.com/cdn2.thecatapi.com/images/d7.gif";
+    document.querySelector('#cat-generator-row-2').appendChild(catImage);
+}
